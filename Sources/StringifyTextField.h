a89be916a7215f2f4ac0f3c79326757ebca14f08
Anton Novichenko <novichenko_a@lwo.by>
2020-04-09 20:44:37 +0300

    Initial commit

---
+++ Sources/StringifyTextField.h
@@ -0,0 +1,19 @@
+//
+//  StringifyTextField.h
+//  StringifyTextField
+//
+//  Created by Anton Novichenko on 4/9/20.
+//  Copyright © 2020 Anton Novichenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringifyTextField.
+FOUNDATION_EXPORT double StringifyTextFieldVersionNumber;
+
+//! Project version string for StringifyTextField.
+FOUNDATION_EXPORT const unsigned char StringifyTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringifyTextField/PublicHeader.h>
+
+
